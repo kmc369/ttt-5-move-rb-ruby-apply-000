c230e0cba4b92994b6df0aa143451b72d347b5bd
keisha coleman <keishacoleman710@gmail.com>
2020-09-16 23:17:13 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,5 +1,8 @@
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def initialize
+  @board = Array.new(9, " ")
+end
+
 def display_board(board)
 
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
