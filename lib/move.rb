7dba525cb5d4d7aa4a0ba67843bad081df25bb05
keisha coleman <keishacoleman710@gmail.com>
2020-09-16 22:38:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,6 +1,7 @@
 
 def display_board(board)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -8,7 +9,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(interger>=0)
+def input_to_index(interger)
    index= interger.to_i - 1
    return index
 end
